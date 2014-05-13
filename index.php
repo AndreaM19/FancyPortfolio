3a24aa572f502e99f8df966ef9f631a1ea856867
AndreaM19 <19andrea.m@gmail.com>
2014-05-13 23:39:10 +0200

    Fancy Portfolio project - L'obiettivo è quello di realizzare un Portfolio/Curriculum vitae elettronico mediante l'utilizzo di una pagina web

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="Andrea Marchetti">
+<link rel="shortcut icon" href="img/icons/favicon/ico.png">
+<title>Andrea Marchetti</title>
+
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.css" rel="stylesheet">
+
+<!-- Custom CSS here -->
+<link href="css/style.css" rel="stylesheet">
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+<script type="text/javascript" language="javascript">
+function screenSize(){
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	
+	var text= 'Width: ' + w + ' Height:' + h;
+	alert(text);
+}
+</script>
+
+</head>
+
+<body>
+
+<div class="container">
+	
+    <!-- Screen dimension info -->
+	<!--<a href="#" class="btn btn-lg btn-default" onClick="return screenSize();">Screen Size</a>-->
+		
+      <div style="margin-top:20px;"></div>
+      <!--
+      <div class="col-md-12 selector text-right" style="margin-bottom:10px;">
+      <ul>
+      	<li>Theme selector: </li>
+      	<a href="#"><li style="color:#9F3">Lime</li></a>
+        <a href="#"><li style="color:#36F">Blue</li></a>
+        <a href="#"><li style="color:#FFF">White</li></a>
+        <a href="#"><li><b style="color:#C60;">F</b><b style="color:#36F;">a</b><b style="color:#9F3;">n</b><b style="color:#C69;">c</b><b style="color:#FFF;">y</b></li></a>
+      </ul>
+      </div>
+      -->
+    
+      <!-- Info boxes -->
+      <div class="col-md-8 info-box">
+      	<div class="info-displayer background-lime">
+        <h1>Andrea Marchetti</h1>
+        <h3>Computer Engineer</h3>
+        <p><b>Short description:</b> Hi, I am a lover of music, technology and photography. I graduated in Computer Engineering and Automation. One of my main passions is my blog, dedicated to the world of Ableton Live, Computer Music, and Synths.</p>
+        <h4>Degree: Computer Engineering and Automation - UNIVPM Ancona</h4>
+        <h4>Diploma: Technical Institute surveyors - ITG Genga Pesaro</h4>
+        </div>
+      </div>
+      
+      <div class="col-md-4 info-box">
+      	<div class="info-displayer background-photo">
+        <!--<h1>Title</h1>-->
+        </div>
+      </div>
+      
+      <div class="col-md-6" style="padding: 0px;">
+          <div class="col-md-4 info-box">
+            <div class="info-displayer background-acqua">
+            <h1>CV</h1>
+            <a href="#"><h4>Click here to see my CV</h4></a>
+            <a href="#"><div class="col-md-3 fa fa-file-text cv-icon" title="Andrea Marchetti CV"></div></a>
+            </div>
+          </div>
+          
+          <div class="col-md-8 info-box">
+            <div class="info-displayer background-pink">
+            <h1>Skills</h1>
+            <ul class="ul-skill">
+            	<li>Web Design</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>PHP</li>
+                <li>Objective programming</li>
+                <li>Java</li>
+                <li>Eclipse IDE</li>
+                <li>Database</li>
+                <li>SQL</li>
+                <li>Microsoft Office Pack</li>
+            </ul>
+            </div>
+          </div>
+          
+          <div class="col-md-12 info-box">
+            <div class="info-displayer background-blue">
+            <h1>Certificates</h1>
+			<h4>Language:</h4>
+				<ul>
+					<li>Trinity College – Graded Examination in Spoken English – Grade 5</li>
+					<li>Trinity College – Graded Examination in Spoken English – Grade 6</li>
+				</ul>
+             <h4>Audio & Music</h4>
+             	<ul>
+					<li>RCF Audio Academy - Indoor acoustic</li>
+					<li>RCF Audio Academy - Acoustic systems for live events</li>
+                    <li>RCF Audio Academy - Acoustic systems for fixed installation</li>
+                    <li>RCF Audio Academy - Home studio recording &amp; monitoring</li>
+				</ul>
+            </div>
+          </div>
+      </div>
+      
+      <div class="col-md-6" style="padding: 0px;">
+      <div class="col-md-12 info-box">
+      	<div class="info-displayer background-orange more-height">
+        <h1>Photography</h1>
+        
+        
+        </div>
+      </div>
+      
+      <div class="col-md-12 info-box">
+      	<div class="info-displayer background-white lower-height">
+        <h1>Social</h1>
+        <a href="#"><div class="col-md-3 fa fa-facebook social-icon" title="Facebook"></div></a>
+        <a href="#"><div class="col-md-3 fa fa-linkedin social-icon" title="LinkedIn"></div></a>
+        <a href="#"><div class="col-md-3 fa fa-flickr social-icon" title="flickr"></div></a>
+        <a href="#"><div class="col-md-3 fa fa-envelope social-icon" title="Mail"></div></a>
+        </div>
+      </div>
+      </div>
+
+	  <div class="col-md-4">
+      <h5 style="color:#AAA;">Electronic fancy CV - Design by Andrea Marchetti</h5>
+      <br>
+      </div>
+    </div> 
+    <!-- /container -->
+
+
+
+    <!-- Le javascript -->
+	<!-- ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+   		<script src="js/jquery/1.11.0/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+  </body>
+</html>
